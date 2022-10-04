fd33281e8dfde993416cea43ea56716a5c9961e1
ics20-1-2022 <113055324+ics20-1-2022@users.noreply.github.com>
2022-10-04 14:05:06 -0400

    fix

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Mr. Coxall
-// Created on: Oct 2020
+// Created on: Sep 2020
 // This file contains the JS functions for index.html
 
 /**
  * This function displays "Hello, World!".
  * @constructor
  */
-function myButtonClicked() {
+ function myButtonClicked() {
   document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
 }
